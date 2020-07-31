981f9c8206b73371686b4d73cf845008a85840ea
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2020-07-31 19:27:12 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "node.cpp"
+
+using namespace std;
+
+
+
+int main()
+{
+	List lista(30, "primeiro");
+	lista.auxInserir(lista.root, 20, "segundo");
+	lista.auxInserir(lista.root, 10, "terceiro");
+//	lista.auxInserir(lista.root, 70, "quarto");
+//	lista.auxInserir(lista.root, 60, "quinto");
+//	lista.auxInserir(lista.root, 80, "sexto");
+
+	//lista.listAll(lista.root);
+	while(1){}
+
+}
